76d889c8470ebcf3a5691bf376022aa592e6a110
Mac Laptop <apple@macs-air.home>
2020-02-17 18:10:38 -0500

    try selenium code

---
+++ src/test/java/Trial/First.java
@@ -0,0 +1,14 @@
+package Trial;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazom.com");
+
+    }
+}
